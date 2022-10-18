30ce3c2a6178b5ee2bc2934a41c64b7f122b7b2a
CampoNover16 <s.mackiewicz00@gmail.com>
2022-10-19 00:21:49 +0200

    Setup Jquery,Sass | new files struct | base.html

--- idea_meal/settings.py
+++ idea_meal/settings.py
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'jquery',
+    'sass_processor',
 ]
 
 MIDDLEWARE = [
@@ -118,6 +120,22 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = 'static'
+MEDIA_ROOT = 'media'
+MEDIA_URL = 'media/'
+
+SASS_PROCESSOR_ROOT = STATIC_ROOT
+
+STATICFILES_DIRS= [
+    BASE_DIR / 'polls' / 'static',
+]
+
+
+STATICFILES_FINDERS = [
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+    'sass_processor.finders.CssFinder',
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
---
+++ polls/static/js/main.js
@@ -0,0 +1,5 @@
+console.log("Say hello to my little friend");
+
+$("#jquery_test").click(() => {
+  console.log("Finally!");
+});
