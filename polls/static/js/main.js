6e600e6749964ef662d6abc02431f8cfcbe34b71
CampoNover16 <s.mackiewicz00@gmail.com>
2022-10-23 15:55:45 +0200

    Custom alerts, web logo, title logo

--- polls/static/js/main.js
+++ polls/static/js/main.js
@@ -1,5 +1,19 @@
-console.log("Say hello to my little friend");
+// console.log("Say hello to my little friend");
 
-$("#jquery_test").click(() => {
-  console.log("Finally!");
-});
+// $("#jquery_test").click(() => {
+//   console.log("Finally!");
+// });
+
+$(document).ready(()=>{
+  $('#test').trigger('click');
+})
+
+function testfunc(){
+  Swal.fire({
+    icon: 'error',
+    title: 'Oops...',
+    text: 'Something went wrong. Try again!',
+    showConfirmButton: false,
+    timer: 2000
+  })
+}
