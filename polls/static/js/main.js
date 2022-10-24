f45284261d13bc874fee5bfbdea4b338c8ef486b
CampoNover16 <s.mackiewicz00@gmail.com>
2022-10-24 21:14:55 +0200

    Miko - navbar

--- polls/static/js/main.js
+++ polls/static/js/main.js
@@ -17,3 +17,22 @@ function testfunc(){
     timer: 2000
   })
 }
+
+$(".navbar").hover(()=>{
+  console.log("test")
+  });
+  
+  let btnContainer = document.getElementById("links");
+  let btns = btnContainer.getElementsByClassName("link");
+  
+  for (let i = 0; i < btns.length; i++) {
+    btns[i].addEventListener("click", function() {
+      let current = document.getElementsByClassName("active");
+  
+      if (current.length > 0) {
+        current[0].className = current[0].className.replace(" active", "");
+      }
+  
+      this.className += " active";
+    });
+  }
