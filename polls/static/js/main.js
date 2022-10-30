eb181f1e473737f598a0e814ebdc5830e892fc19
CampoNover16 <s.mackiewicz00@gmail.com>
2022-10-30 21:27:59 +0100

    Miko - navbar improvments

--- polls/static/js/main.js
+++ polls/static/js/main.js
@@ -18,21 +18,23 @@ function testfunc(){
   })
 }
 
-$(".navbar").hover(()=>{
-  console.log("test")
+//function for hiding navbar
+$("#navBtn").on("click",() => {
+  document.getElementById("nav").classList.toggle("short");
+})
+
+//function for buttons
+let btnContainer = document.getElementById("links");
+let btns = btnContainer.getElementsByClassName("link");
+
+for (let i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function() {
+    let current = document.getElementsByClassName("active");
+
+    if (current.length > 0) {
+      current[0].className = current[0].className.replace(" active", "");
+    }
+
+    this.className += " active";
   });
-  
-  let btnContainer = document.getElementById("links");
-  let btns = btnContainer.getElementsByClassName("link");
-  
-  for (let i = 0; i < btns.length; i++) {
-    btns[i].addEventListener("click", function() {
-      let current = document.getElementsByClassName("active");
-  
-      if (current.length > 0) {
-        current[0].className = current[0].className.replace(" active", "");
-      }
-  
-      this.className += " active";
-    });
-  }
+}
