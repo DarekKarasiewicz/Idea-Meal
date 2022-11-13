a665a970ec9f59ac576b63394c3bd768141403a5
CampoNover16 <s.mackiewicz00@gmail.com>
2022-11-13 01:09:30 +0100

    login err mess, miko nav fix, theme changer slider

--- polls/static/js/main.js
+++ polls/static/js/main.js
@@ -1,12 +1,11 @@
 $(document).ready(()=>{
-  $('#test').trigger('click');
-  $('#disable_nav').on('click',()=>{
-    $('#nav').show();
-  })
+  $('#error_trigger').trigger('click');
+
   $('#nav_reciper_hidder').on('click',()=>{
     $('#nav_recipe_res_div').hide("fast");
     $('#nav_reciper_show').show("fast");
   })
+
   $('#nav_reciper_show').on('click',()=>{
     $('#nav_recipe_res_div').show("fast");
     $('#nav_reciper_show').hide("fast");
@@ -21,7 +20,7 @@ $('.recipe_box_simple').on('click',function(){
   }
 })
 
-function testfunc(){
+function errorMessage(){
   Swal.fire({
     icon: 'error',
     title: 'Oops...',
@@ -51,3 +50,23 @@ for (let i = 0; i < btns.length; i++) {
     this.className += " active";
   });
 }
+$('.navbar__toggle_btn').on('click',function(){
+  if($('#nav').hasClass('short')){
+    $('.switch-box_wrapper').hide();
+  }else{
+    $('.switch-box_wrapper').show();
+  }
+})
+
+//theme switcher controller
+$('#theme_switcher').on('change',function(){
+  if($(this).find('input').is(':checked')){
+    $('#checkbox_sun').hide('fast');
+    $('#checkbox_moon').show('fast');
+    console.log('checked true');
+  }else{
+    $('#checkbox_moon').hide('fast');
+    $('#checkbox_sun').show('fast');
+    console.log('checked false');
+  }
+})
