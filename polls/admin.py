a967fb2c8ad6156de62ff794e52cd86ba04ab91a
Darek Karasiewicz <dar.karasiewicz@gmail.com>
2022-11-16 21:13:35 +0100

    Fill admin page to missings db field

--- polls/admin.py
+++ polls/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
 # Register your models here.
-from .models import Fridge, Product, Recipes, Fridge_products_counts
+from .models import Fridge, Product, Recipes, Fridge_products_counts, Cuisine_category, Meal_time_category, Products_category, Comment
 
 admin.site.register(Fridge)
 admin.site.register(Product)
 admin.site.register(Recipes)
 admin.site.register(Fridge_products_counts)
+admin.site.register(Cuisine_category)
+admin.site.register(Comment)
+admin.site.register(Products_category)
+admin.site.register(Meal_time_category)
