2fb1153dd8dac9ae64ad150eb82bd1e79725883f
Darek Karasiewicz <dar.karasiewicz@gmail.com>
2022-11-21 20:49:54 +0100

    Create algorithm to create shopping list
    Algorithm take want recipes you want and create base on them and base on
    your fridge shopping list in future i want add option to turn off based
    on fridge because user can use that option without using fridge

--- polls/views.py
+++ polls/views.py
@@ -1,31 +1,41 @@
-from strenum import StrEnum #Python 3.11 = from enum
+from strenum import StrEnum  # Python 3.11 = from enum
 from enum import auto
 
-#DJANGO IMPORTS
+# DJANGO IMPORTS
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, HttpResponseRedirect, Http404
 from django.views.decorators.csrf import csrf_exempt
 from django.contrib.auth.models import User
-from django.contrib.auth import authenticate, login,logout
+from django.contrib.auth import authenticate, login, logout
 from django.urls import reverse
 from django.contrib.auth.decorators import login_required
 
-#MODELS IMPORTS
-from .models import Product,Comment,Recipe,Fridge,Fridge_products_counts,Comments_to_Recipe
+# MODELS IMPORTS
+from .models import (
+    Product,
+    Comment,
+    Recipe,
+    Fridge,
+    Fridge_products_counts,
+    Comments_to_Recipe,
+    Recipe_products_counts,
+)
 
-#Here puts all cuisine category
+# Here puts all cuisine category
 class Cuisine_category(StrEnum):
     POLAND = auto()
     AMERICAN = auto()
     ASIAN = auto()
 
-#Here puts all Meal_time_category
+
+# Here puts all Meal_time_category
 class Meal_time_category(StrEnum):
     BREAKFAST = auto()
     DINER = auto()
     SUPPER = auto()
 
-#Here puts all Products_category
+
+# Here puts all Products_category
 class Product_category(StrEnum):
     MEAT = auto()
     FISH = auto()
@@ -33,17 +43,21 @@ class Product_category(StrEnum):
     FRUIT = auto()
     VEGETABLES = auto()
 
+
 class Product_unit(StrEnum):
     ML = auto()
     GRAMS = auto()
     UNIT = auto()
 
+
 def welcome_page(request):
-    return HttpResponseRedirect('login')
+    return HttpResponseRedirect("login")
+
 
 def logout_view(request):
     logout(request)
-    return HttpResponseRedirect('login')
+    return HttpResponseRedirect("login")
+
 
 def register_page(request):
     if request.method == "POST":
@@ -59,12 +73,35 @@ def register_page(request):
 
     return render(request, "polls/register_page.html")
 
+
 def find_recipe_base_on_products(product: dict) -> Recipe:
     pass
 
+
 def create_shopping_list(list_of_recipes: list) -> list:
     dict_of_products = {}
-    return list_of_products
+    fridge_products = Fridge_products_counts.objects.all()
+    for recipe in list_of_recipes:
+        products = Recipe_products_counts.objects.filter(recipe=recipe.id)
+        if len(products) < 1:
+            raise Http404
+        for product in products:
+            if product.product in dict_of_products:
+                dict_of_products[product.product] += product.ammount
+            else:
+                dict_of_products[product.product] = product.ammount
+    for f_product in fridge_products:
+        if f_product.product in dict_of_products:
+            if (dict_of_products[f_product.product] - f_product.ammount) <= 0:
+                del dict_of_products[f_product.product]
+
+            else:
+                dict_of_products[f_product.product] = (
+                    dict_of_products[f_product.product] - f_product.ammount
+                )
+
+    return dict_of_products
+
 
 @csrf_exempt
 def login_page(request):
@@ -72,28 +109,36 @@ def login_page(request):
         auth_login = request.POST["auth_login"]
         auth_password = request.POST["auth_password"]
         if user := authenticate(username=auth_login, password=auth_password):
-            login(request,user)
+            login(request, user)
             return HttpResponseRedirect(reverse("main", args=(user.id,)))
         else:
             return HttpResponse("Sorry ale coś ci chyba nie poszło !")
     return render(request, "polls/login_page.html")
 
+
 @login_required
-def main_page(request,user_id):
-    if int(request.session['_auth_user_id']) != int(user_id):
+def main_page(request, user_id):
+    tmp = [
+        get_object_or_404(Recipe, pk=1),
+        get_object_or_404(Recipe, pk=2),
+        get_object_or_404(Recipe, pk=3),
+    ]
+    print(create_shopping_list(tmp))
+    if int(request.session["_auth_user_id"]) != int(user_id):
         raise Http404
-    session_user = get_object_or_404(User,
-                                     pk=int(request.session['_auth_user_id']))
+    session_user = get_object_or_404(User, pk=int(request.session["_auth_user_id"]))
     all_recipes = Recipe.objects.all()
     all_products = Product.objects.all()
-    return render(request,"polls/main_page.html",{'user':session_user
-                                                 ,'recipes': all_recipes
-                                                 ,'products': all_products
-                                                  })
+    return render(
+        request,
+        "polls/main_page.html",
+        {"user": session_user, "recipes": all_recipes, "products": all_products},
+    )
+
 
 @login_required
 def add_recipes(request):
-    session_user = get_object_or_404(User, pk=int(request.session['_auth_user_id']))
+    session_user = get_object_or_404(User, pk=int(request.session["_auth_user_id"]))
     if request.method == "POST":
         name = request.POST["recipe_name"]
         description = request.POST["description"]
@@ -101,19 +146,23 @@ def add_recipes(request):
         cuisine_category = request.POST["cuisine_category"]
         meal_time_category = request.POST["meal_time_category"]
         prepare_time_post = request.POST["prepare_time"].split(":")
-        prepare_time = int(prepare_time_post[0])*60 + int(prepare_time_post[1])
+        prepare_time = int(prepare_time_post[0]) * 60 + int(prepare_time_post[1])
         spiciness = request.POST["spiciness"]
         per_serving = request.POST["per_serving"]
         is_verificated = False
 
-        #YES I KNOW IT WILL WORK DIFRENTLY
-        enum_cuisine_category= [e.value for e in Cuisine_category if
-                                str(e.value).lower()
-                                ==cuisine_category.lower()]
-        enum_meal_time_category= [e.value for e in Meal_time_category if
-                                str(e.value).lower()
-                                  ==meal_time_category.lower()]
-        if(len(enum_meal_time_category) == 0 and len(enum_cuisine_category) == 0):
+        # YES I KNOW IT WILL WORK DIFRENTLY
+        enum_cuisine_category = [
+            e.value
+            for e in Cuisine_category
+            if str(e.value).lower() == cuisine_category.lower()
+        ]
+        enum_meal_time_category = [
+            e.value
+            for e in Meal_time_category
+            if str(e.value).lower() == meal_time_category.lower()
+        ]
+        if len(enum_meal_time_category) == 0 and len(enum_cuisine_category) == 0:
             raise Http404
 
         match difficulty:
@@ -126,35 +175,34 @@ def add_recipes(request):
             case _:
                 raise Http404
 
-        recipes = Recipe(name = name
-                          , description = description
-                          , difficulty = difficulty
-                          , prepare_time = prepare_time
-                          , spiciness= spiciness
-                          , is_verificated = is_verificated
-                          , cuisine_category = enum_cuisine_category[0]
-                          , meal_time_category = enum_meal_time_category[0]
-                          , per_serving = per_serving
-                          )
+        recipes = Recipe(
+            name=name,
+            description=description,
+            difficulty=difficulty,
+            prepare_time=prepare_time,
+            spiciness=spiciness,
+            is_verificated=is_verificated,
+            cuisine_category=enum_cuisine_category[0],
+            meal_time_category=enum_meal_time_category[0],
+            per_serving=per_serving,
+        )
         recipes.save()
         return HttpResponseRedirect(reverse("main", args=(session_user.id,)))
-    return render(request, "polls/recipes_page.html", {'user_id':
-                                                       session_user.id})
+    return render(request, "polls/recipes_page.html", {"user_id": session_user.id})
+
 
 @login_required
-def recipes_page(request,recipe_id):
-    session_user = get_object_or_404(User, pk=int(request.session['_auth_user_id']))
+def recipes_page(request, recipe_id):
+    session_user = get_object_or_404(User, pk=int(request.session["_auth_user_id"]))
     recipe = get_object_or_404(Recipe, pk=recipe_id)
 
     if request.method == "POST":
         raiting = request.POST["raiting"]
         comment_text = request.POST["comment"]
-        comment = Comment(raiting=raiting, description=comment_text,
-                          user=session_user )
+        comment = Comment(raiting=raiting, description=comment_text, user=session_user)
         comment.save()
-        tmf=Comments_to_Recipe(recipe=recipe,comment=comment)
+        tmf = Comments_to_Recipe(recipe=recipe, comment=comment)
         tmf.save()
-
 
     test_show = recipe.__dict__
     test_show1 = Comments_to_Recipe.objects.all()
@@ -163,29 +211,36 @@ def recipes_page(request,recipe_id):
         if x.recipe.id == recipe.id:
             all_comments.append(x)
 
-    return render(request, "polls/recipe_view.html", {'recipe': test_show
-                                                      ,'all_comments':all_comments})
+    return render(
+        request,
+        "polls/recipe_view.html",
+        {"recipe": test_show, "all_comments": all_comments},
+    )
+
 
 @login_required
 def product_page(request):
-    session_user = get_object_or_404(User, pk=int(request.session['_auth_user_id']))
+    session_user = get_object_or_404(User, pk=int(request.session["_auth_user_id"]))
     product_category = [e.value for e in Product_category]
     print(product_category)
     if request.method == "POST":
         name = request.POST["product_name"]
         product_category_post = request.POST["product_name"]
 
-        product = Product(name = name, product_category = product_category_post)
+        product = Product(name=name, product_category=product_category_post)
         product.save()
         return HttpResponseRedirect(reverse("main", args=(session_user.id,)))
 
-    return render(request, "polls/product_page.html", {'user_id':
-                                                       session_user.id,
-                                                       'product_categories':product_category})
+    return render(
+        request,
+        "polls/product_page.html",
+        {"user_id": session_user.id, "product_categories": product_category},
+    )
+
 
 @login_required
-def user_fridge(request,user_id):
-    session_user = get_object_or_404(User, pk=int(request.session['_auth_user_id']))
+def user_fridge(request, user_id):
+    session_user = get_object_or_404(User, pk=int(request.session["_auth_user_id"]))
     fridge = get_object_or_404(Fridge, user=session_user)
     all_products = Product.objects.all()
     product_in_fridge = Fridge_products_counts.objects.all()
@@ -193,13 +248,17 @@ def user_fridge(request,user_id):
         product_name = request.POST["product_name"]
         product = get_object_or_404(Product, name=product_name)
         product_number = request.POST["product_number"]
-        fridge_product = Fridge_products_counts(product=product,
-                                                item_count = product_number,
-                                                fridge=fridge)
+        fridge_product = Fridge_products_counts(
+            product=product, ammount=product_number, fridge=fridge
+        )
         fridge_product.save()
 
-    return render(request, "polls/fridge.html", {'user_id': session_user.id,
-                                                 'products': all_products,
-                                                 'product_in_fridge':product_in_fridge,
-                                                 })
-
+    return render(
+        request,
+        "polls/fridge.html",
+        {
+            "user_id": session_user.id,
+            "products": all_products,
+            "product_in_fridge": product_in_fridge,
+        },
+    )
