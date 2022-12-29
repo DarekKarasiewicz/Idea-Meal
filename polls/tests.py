1560d3c0760f50342c36341c66aa818ada6621bb
Darek Karasiewicz <dar.karasiewicz@gmail.com>
2022-12-29 15:22:18 +0100

    WIP

--- polls/tests.py
+++ polls/tests.py
@@ -1,3 +1,31 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
 
-# Create your tests here.
+# MODELS IMPORTS
+from .models import (
+    Product,
+    Comment,
+    Recipe,
+    Fridge,
+    Fridge_products_counts,
+    Comments_to_Recipe,
+    Recipe_products_counts,
+)
+
+class UserTestCase(TestCase):
+    def setUp(self):
+        user = User.objects.create_user(username="Forbiden_name",
+                            email="notyourbuisnes@oo.com",
+                            password = "Fantasy123!")
+        user_fridge = Fridge(user = user.id)
+        product = Product(user=user.id, name =
+                                         "test_product")
+
+
+    def test_user_fridge_test(self):
+        self.assertIsNotNone(user_fridge.objects.filter(user = user.id))
+
+    def test_user_can_add_product(self):
+        test_product_in_fridge = Fridge_products_counts(product= product, ammount=1, fridge=
+                               user_fridge.id)
+        self.assertEqual(Fridge_products_counts.objects.filter(fridge.user_fridge.id).id,test_product_in_fridge.id)
