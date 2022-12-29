282cf337d84ae07a2a7bc99bfdf27b5bb831dc8f
Darek Karasiewicz <dar.karasiewicz@gmail.com>
2022-12-29 17:08:33 +0100

    Create testing database only for test

--- idea_meal/settings.py
+++ idea_meal/settings.py
@@ -1,3 +1,4 @@
+import sys
 """
 Django settings for idea_meal project.
 
@@ -77,17 +78,32 @@ WSGI_APPLICATION = 'idea_meal.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'mbbdvmhu',
-        'USER': 'mbbdvmhu',
-        'PASSWORD': 'Os3wA7JMVXYleQEA-ZPKSAvxzITnJ-xN',
-        'HOST': 'lucky.db.elephantsql.com',
-        'PORT': '5432',
+is_test = "manage.py" in sys.argv and "test" in sys.argv
+if is_test:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': 'karas',
+            'USER': 'karas',
+            'PASSWORD': '',
+            'HOST': '127.0.0.1',
+            'PORT': '5432',
+            }
+        }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': 'mbbdvmhu',
+            'USER': 'mbbdvmhu',
+            'PASSWORD': 'Os3wA7JMVXYleQEA-ZPKSAvxzITnJ-xN',
+            'HOST': 'lucky.db.elephantsql.com',
+            'PORT': '5432',
+            'TEST': {
+                'NAME': 'test_karas',
+            },
+        }
     }
-}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
